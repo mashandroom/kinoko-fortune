5f45a6bc1cdd53c2f9aee89df58a6441d9891f28
h2m_kinoko <happy2mylife@gmail.com>
2020-01-04 17:55:25 +0900

    update ui.

--- docs/kinoko.js
+++ docs/kinoko.js
@@ -1,29 +1,90 @@
 const resultList =
     [
-        'chuukichi.png',
-        'daikichi.png',
-        'daikyou.png',
-        'kichi.png',
-        'kyou.png',
-        'shoukichi.png',
-        'suekichi.png',
+        {
+            name: '中吉',
+            img: 'chuukichi.png',
+        },
+        {
+            name: '大吉',
+            img: 'daikichi.png',
+        },
+        {
+            name: '大凶',
+            img: 'daikyou.png',
+        },
+        {
+            name: '吉',
+            img: 'kichi.png',
+        },
+        {
+            name: '凶',
+            img: 'kyou.png',
+        },
+        {
+            name: '小吉',
+            img: 'shoukichi.png',
+        },
+        {
+            name: '末吉',
+            img: 'suekichi.png',
+        },
     ]
+let resultId = 0
 const luckyKinokoList = [
-    'enokidake.png',
-    'eringi.png',
-    'kikurage.png',
-    'kinokonoyama.png',
-    'maitake.png',
-    'masicmushroom.png',
-    'matsutake.png',
-    'mushroom.png',
-    'nameko.png',
-    'shiitake.png',
-    'shimeji.png',
-    'toryufu.png',
+    {
+        name: 'えのきだけ',
+        img: 'enokidake.png',
+    },
+    {
+        name: 'エリンギ',
+        img: 'eringi.png',
+    },
+    {
+        name: 'きくらげ',
+        img: 'kikurage.png',
+    },
+    {
+        name: 'きのこの山',
+        img: 'kinokonoyama.png',
+    },
+    {
+        name: 'まいたけ',
+        img: 'maitake.png',
+    },
+    {
+        name: 'マジックマッシュルーム',
+        img: 'masicmushroom.png',
+    },
+    {
+        name: 'まつたけ',
+        img: 'matsutake.png',
+    },
+    {
+        name: 'マッシュルーム',
+        img: 'mushroom.png',
+    },
+    {
+        name: 'なめこ',
+        img: 'nameko.png',
+    },
+    {
+        name: 'しいたけ',
+        img: 'shiitake.png',
+    },
+    {
+        name: 'しめじ',
+        img: 'shimeji.png',
+    },
+    {
+        name: 'トリュフ',
+        img: 'toryufu.png',
+    }
 ]
 
+let kinokoId = 0
+
 function init() {
+    console.log(`${resultList.length}`)
     fortuneBgm.addEventListener('ended', () => {
         ponBgm.play();
         setTimeout(showResult, 2000);
@@ -41,11 +102,29 @@ function startFortune() {
 function showResult() {
     fortuneBoxAnimation.classList.add("none");
     result.classList.remove('none')
-    const resultId = Math.floor(Math.random() * resultList.length)
-    resultImage.src = `./res/img/result/${resultList[resultId]}`
-    const kinokoId = Math.floor(Math.random() * luckyKinokoList.length)
-    luckyKinoko.src = `./res/img/kinoko/${luckyKinokoList[kinokoId]}`
+    resultId = Math.floor(Math.random() * resultList.length)
+    resultImage.src = `./res/img/result/${resultList[resultId].img}`
+    kinokoId = Math.floor(Math.random() * luckyKinokoList.length)
+    luckyKinoko.src = `./res/img/kinoko/${luckyKinokoList[kinokoId].img}`
 
 }
 
+function tweet(body) {
+    tweetLink = document.getElementById('tweetLink');
+    tweetLink.href = `https://twitter.com/share?text=【キノコ】私の今年のラッキーキノコは、${luckyKinokoList[kinokoId].name}です。美味しく頂きます。&url=https://mashandroom.github.io/kinoko-fortune/&hashtags=菌駕信念,キノコ`;
+    tweetLink.click();
+}
+
+function shareFB() {
+
+    FB.ui({
+        method: 'feed',
+        link: '' + 'https://mashandroom.github.io/kinoko-fortune/',
+        display: 'popup',
+        quote: `【キノコ】私の今年のラッキーキノコは、${luckyKinokoList[kinokoId].name}です。`
+    }, function (res) {
+        //返答をログに出力
+        console.log(res);
+    });
+}
 window.onload = init;
